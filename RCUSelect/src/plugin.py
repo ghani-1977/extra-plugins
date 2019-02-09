030df0f14e1c1f51b335843899673b82871870d4
Persian Prince <persianpros@gmail.com>
2019-02-09 18:47:32 +0330

    Complete rework of RCU Select plugin:
    - No more WeTek specific, be general so we could use it for all amlogic devices
    - Support all of our remote conf files: https://github.com/OpenVisionE2/amremote

--- RCUSelect/src/plugin.py
+++ RCUSelect/src/plugin.py
@@ -48,33 +48,53 @@ class RCUSelect(Screen):
 		title = _("RCU Select")
 		self.setTitle(title)
 		self["pixmap"] = Pixmap()
-		self.rcuval = [_("WeTek Play2 RCU"),
-		_('WeTek Play (Classic) RCU'),
-		_("WeTek Play Enigma2 RCU"),
-		_("WeTek Play OpenElec RCU"),
-		_("AB IPBox 9900/99/55 HD RCU"),
-		_("Alien2/1 RCU"),
-		_("Alien1 old RCU"),
-		_("GI LX3 RCU"),
-		_("Gigablue 800 UE Plus RCU"),
-		_("Mutant HD2400 RCU"),
-		_("Octagon SF8 RCU"),
-		_("Technomate Nano RCU"),
-		_("xtrend ET10000 RCU"),
-		_("Zgemma Star RCU")]
+		self.rcuval = [_("A400 NEC remote"),
+		_("Alien 1 Old HOF54B1-4 remote"),
+		_("Alien 2 New HOF55D remote"),
+		_("Alien 5 amlogic NEC remote"),
+		_("Factory amlogic NEC remote"),
+		_("GigaBlue 800 UE Plus remote"),
+		_("Golden Interstar LX3 remote"),
+		_("K1 Plus amlogic NEC remote"),
+		_("K1 Pro amlogic NEC remote"),
+		_("M8S ZAP-A10 NEC remote"),
+		_("M8S Plus amlogic NEC remote"),
+		_("MB2 amlogic M8B remote"),
+		_("MX3G amlogic MXIII-G remote"),
+		_("MX Pro 2 amlogic MXproII remote"),
+		_("MXQV20 ZAP-A10 NEC remote"),
+		_("MXQV31 amlogic NEC remote"),
+		_("Octagon SF8 remote"),
+		_("Qintex Q812 remote"),
+		_("CVT RC5 remote"),
+		_("RCMM amlogic NEC remote"),
+		_("TX1 amlogic NEC remote"),
+		_("TX3 Pro amlogic NEC remote"),
+		_("TX5 Pro amlogic NEC remote"),
+		_("Vander amlogic remote"),
+		_("Wechip V5 amlogic remote"),
+		_("WeTek Play NEC remote"),
+		_("WeTek Play (Classic) remote"),
+		_("WeTek Play Enigma2 remote"),
+		_("WeTek OpenELEC NEC remote"),
+		_("Xtrend ET10000 remote"),
+		_("Mutant HD2400 remote"),
+		_("AB IPBox 9900/99/55HD remote"),
+		_("WeTek Play 2 NEC remote"),
+		_("Technomate Nano remote"),
+		_("X96 amlogic remote"),
+		_("X98 amlogic remote"),
+		_("Zgemma Star remote")]
 		self.SetOSDList()
 		self.MakeKeymapBckUp()
 
 	def MakeKeymapBckUp(self):
-		filename = '/usr/lib/enigma2/python/Plugins/Extensions/RCUSelect/conf/keymap.orig.xml'
-		cmd ='cp -f /usr/share/enigma2/keymap.xml ' + filename + ' &'
+		filename = "/usr/lib/enigma2/python/Plugins/Extensions/RCUSelect/conf/keymap.orig.xml"
+		cmd ="cp -f /usr/share/enigma2/keymap.xml " + filename + " &"
 		if not os.path.exists(filename):
 			os.system(cmd)
 
 	def SetOSDList(self):
-		boxime = HardwareInfo().get_device_name()
-		if boxime == 'wetekplay2': choice = 'WeTek Play2 RCU'
-		if boxime == 'wetekplay': choice = 'WeTek Play Enigma2 RCU'
 		try:
 			choice = open("/etc/amremote/.choice", "r").read()
 		except IOError:
@@ -100,51 +120,99 @@ class RCUSelect(Screen):
 			self.rcuv = self.rcuval[var]
 			#if self.rcuv != self.rcuold: copy keymap
 			try:
-				if self.rcuv == 'WeTek Play2 RCU':
-					os.system("cp -f /etc/amremote/wetek_play2.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'WeTek Play (Classic) RCU':
-					os.system("cp -f /etc/amremote/wetek1.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'WeTek Play OpenElec RCU':
-					os.system("cp -f /etc/amremote/wetek3.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'AB IPBox 9900/99/55 HD RCU':
-					os.system("cp -f /etc/amremote/wetek_ipbox9900remote.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Alien2/1 RCU':
-					os.system("cp -f /etc/amremote/alien2.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Alien1 old RCU':
-					os.system("cp -f /etc/amremote/alien.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'GI LX3 RCU':
-					os.system("cp -f /etc/amremote/gilx3.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Gigablue 800 UE Plus RCU':
-					os.system("cp -f /etc/amremote/gb800ueplus.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Mutant HD2400 RCU':
-					os.system("cp -f /etc/amremote/wetek_hd2400remote.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Octagon SF8 RCU':
-					os.system("cp -f /etc/amremote/octagonsf8.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Technomate Nano RCU':
-					os.system("cp -f /etc/amremote/wetek_tmnanoremote.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'xtrend ET10000 RCU':
-					os.system("cp -f /etc/amremote/wetek_et10000remote.conf /etc/amremote/wetek.conf &")
-				elif self.rcuv == 'Zgemma Star RCU':
-					os.system("cp -f /etc/amremote/zgemmastar.conf /etc/amremote/wetek.conf &")
+				if self.rcuv == "A400 NEC remote":
+					os.system("cp -f /etc/amremote/a400.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Alien 1 Old HOF54B1-4 remote":
+					os.system("cp -f /etc/amremote/alien.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Alien 2 New HOF55D remote":
+					os.system("cp -f /etc/amremote/alien2.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Alien 5 amlogic NEC remote":
+					os.system("cp -f /etc/amremote/alien5.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Factory amlogic NEC remote":
+					os.system("cp -f /etc/amremote/factory_remote.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "GigaBlue 800 UE Plus remote":
+					os.system("cp -f /etc/amremote/gb800ueplus.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Golden Interstar LX3 remote":
+					os.system("cp -f /etc/amremote/gilx3.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "K1 Plus amlogic NEC remote":
+					os.system("cp -f /etc/amremote/k1plus.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "K1 Pro amlogic NEC remote":
+					os.system("cp -f /etc/amremote/k1pro.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "M8S ZAP-A10 NEC remote":
+					os.system("cp -f /etc/amremote/m8s.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "M8S Plus amlogic NEC remote":
+					os.system("cp -f /etc/amremote/m8splus.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "MB2 amlogic M8B remote":
+					os.system("cp -f /etc/amremote/mb2.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "MX3G amlogic MXIII-G remote":
+					os.system("cp -f /etc/amremote/mx3g.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "MX Pro 2 amlogic MXproII remote":
+					os.system("cp -f /etc/amremote/mxpro2.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "MXQV20 ZAP-A10 NEC remote":
+					os.system("cp -f /etc/amremote/mxqv20.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "MXQV31 amlogic NEC remote":
+					os.system("cp -f /etc/amremote/mxqv31.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Octagon SF8 remote":
+					os.system("cp -f /etc/amremote/octagonsf8.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Qintex Q812 remote":
+					os.system("cp -f /etc/amremote/qintex.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "CVT RC5 remote":
+					os.system("cp -f /etc/amremote/rc5.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "RCMM amlogic NEC remote":
+					os.system("cp -f /etc/amremote/remotercmm.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "TX1 amlogic NEC remote":
+					os.system("cp -f /etc/amremote/tx1.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "TX3 Pro amlogic NEC remote":
+					os.system("cp -f /etc/amremote/tx3pro.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "TX5 Pro amlogic NEC remote":
+					os.system("cp -f /etc/amremote/tx5pro.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Vander amlogic remote":
+					os.system("cp -f /etc/amremote/vander.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Wechip V5 amlogic remote":
+					os.system("cp -f /etc/amremote/wechipv5.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "WeTek Play NEC remote":
+					os.system("cp -f /etc/amremote/wetek0.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "WeTek Play (Classic) remote":
+					os.system("cp -f /etc/amremote/wetek1.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "WeTek Play Enigma2 remote":
+					os.system("cp -f /etc/amremote/wetek2.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "WeTek OpenELEC NEC remote":
+					os.system("cp -f /etc/amremote/wetek3.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Xtrend ET10000 remote":
+					os.system("cp -f /etc/amremote/wetek_et10000remote.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Mutant HD2400 remote":
+					os.system("cp -f /etc/amremote/wetek_hd2400remote.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "AB IPBox 9900/99/55HD remote":
+					os.system("cp -f /etc/amremote/wetek_ipbox9900remote.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "WeTek Play 2 NEC remote":
+					os.system("cp -f /etc/amremote/wetek_play2.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Technomate Nano remote":
+					os.system("cp -f /etc/amremote/wetek_tmnanoremote.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "X96 amlogic remote":
+					os.system("cp -f /etc/amremote/x96.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "X98 amlogic remote":
+					os.system("cp -f /etc/amremote/x98.conf /etc/amremote/rcuselect.conf &")
+				elif self.rcuv == "Zgemma Star remote":
+					os.system("cp -f /etc/amremote/zgemmastar.conf /etc/amremote/rcuselect.conf &")
 				else:
-					os.system("cp -f /etc/amremote/wetek2.conf /etc/amremote/wetek.conf &")
+					os.system("cp -f /etc/amremote/wetek2.conf /etc/amremote/rcuselect.conf &")
 				f = open("/etc/amremote/.choice", "w")
 				f.write(self.rcuv)
 				f.close()
 				os.system("killall -9 remotecfg &")
 				boxime = HardwareInfo().get_device_name()
-				if boxime == 'wetekplay2':
-					fin = file('/etc/amremote/wetek.conf')
-					fout = open('/etc/amremote/wetek_tmp.conf', 'w')
+				if boxime == "wetekplay2":
+					fin = file("/etc/amremote/rcuselect.conf")
+					fout = open("/etc/amremote/rcuselect_tmp.conf", "w")
 					for line in fin :
-						if 'work_mode' in line: line = 'work_mode  	= 0\n'
+						if "work_mode" in line: line = "work_mode  	= 0\n"
 						fout.write(line)
 					fout.close()
-					os.system('mv -f /etc/amremote/wetek_tmp.conf /etc/amremote/wetek.conf &')
-				os.system("/usr/bin/remotecfg /etc/amremote/wetek.conf &")
-				if self.rcuold == "WeTek Play OpenElec RCU" or self.rcuv == "WeTek Play OpenElec RCU":
+					os.system("mv -f /etc/amremote/rcuselect_tmp.conf /etc/amremote/rcuselect.conf &")
+				os.system("/usr/bin/remotecfg /etc/amremote/rcuselect.conf &")
+				if self.rcuold == "WeTek OpenELEC NEC remote" or self.rcuv == "WeTek OpenELEC NEC remote":
 					if self.rcuold != self.rcuv:
-						if self.rcuv == 'WeTek Play OpenElec RCU':
+						if self.rcuv == "WeTek OpenELEC NEC remote":
 							os.system("cp -f /usr/lib/enigma2/python/Plugins/Extensions/RCUSelect/conf/keymap.OE.xml /usr/share/enigma2/keymap.xml &")
 						else:
 							os.system("cp -f /usr/lib/enigma2/python/Plugins/Extensions/RCUSelect/conf/keymap.orig.xml /usr/share/enigma2/keymap.xml &")
@@ -168,10 +236,4 @@ def system(menuid):
 		return []
         
 def Plugins(**kwargs):
-	boxime = HardwareInfo().get_device_name()
-	if boxime == 'wetekplay' or boxime == 'wetekplayplus' or boxime == 'wetekplay2' or boxime == 'wetekplay2s' :
-		return \
-			[PluginDescriptor(name=_("RCU Select"), where = PluginDescriptor.WHERE_MENU, fnc=system),
-			]
-	else:
-		return []
+	return PluginDescriptor(name=_("RCU Select"), where = PluginDescriptor.WHERE_MENU, fnc=system)
