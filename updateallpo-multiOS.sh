fc78805e1c58823a866bd03998bee35845adb277
Persian Prince <persianpros@yahoo.com>
2019-05-31 14:33:53 +0430

    Update po files using updateallpo-multiOS.sh

--- updateallpo-multiOS.sh
+++ updateallpo-multiOS.sh
@@ -229,3 +229,4 @@ printf "           3) unselect keep original file format\n"
 printf "You only need to do this once in PoEdit.\n\n"
 printf "Please read the translators wiki page:\n"
 printf "\nhttps://wiki.openpli.org/Information_for_Translators\n"
+find . -name "*.mo" -type f -delete
