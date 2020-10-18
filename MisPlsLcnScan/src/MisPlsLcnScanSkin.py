ddcc21fe56340c0f032c3d3a9f133f5fb893bae7
persianpros <persianpros@gmail.com>
2020-10-18 13:53:02 +0330

    Add missing floor division for python 2
    In Python 3.x, 5 / 2 will return 2.5 and 5 // 2 will return 2. The former is floating point division, and the latter is floor division, sometimes also called integer division.
    
    In Python 2.2 or later in the 2.x line, there is no difference for integers unless you perform a from __future__ import division, which causes Python 2.x to adopt the 3.x behavior.

--- MisPlsLcnScan/src/MisPlsLcnScanSkin.py
+++ MisPlsLcnScan/src/MisPlsLcnScanSkin.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-#
+from __future__ import division
 # Note for people skinning the scan screen...
 #
 # There are six skinable outputs as follows:
--- TerrestrialScan/src/MakeBouquet.py
+++ TerrestrialScan/src/MakeBouquet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+from __future__ import division, print_function
 # for localized messages
 from . import _
 
